e0b4e40e1c08cce236af1a20dda844d3b2ec783f
MukilKumar <mukilselvam27@gmail.com>
2022-04-21 12:26:45 +0530

    first commit

---
+++ src/app/pages/new-task/new-task.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.scss']
+})
+export class NewTaskComponent implements OnInit {
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+  }
+  createTask(title: string, listId: string){
+
+
+  }
+}
---
+++ src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists: any;
+  tasks: any;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params)=>{
+
+      this.taskService.getTasks(params['listId']).subscribe((tasks: any)=>{
+        this.tasks = tasks
+
+      })
+
+
+    } )
+
+    this.taskService.getLists().subscribe((lists: any)=>{
+      this.lists = lists;
+      // console.log(lists)
+
+    })
+  }
+
+
+
+ 
+     
+  
+}
